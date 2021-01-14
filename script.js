850097624350d14aed8e44b5541ad0a87a460e22
denstojkov <denstojkov@hotmail.com>
2021-01-14 12:24:06 +0100

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke()
 
-function generateJoke(){
+async function generateJoke(){
 
     const config = {
         headers: {
@@ -11,9 +11,11 @@ function generateJoke(){
         }
     }
 
-    fetch("https://icanhazdadjoke.com", config).then(res => res.json()).then(data => {
-        jokeEl.innerHTML = data.joke;
-    })
+    const res = await fetch("https://icanhazdadjoke.com", config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
 }
 
 jokeBtn.addEventListener("click", generateJoke)
